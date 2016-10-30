248f1fe894a307baf1c44af0010efcb8c0512154
Makoa Jacobsen <makoa@makoajacobsen.com>
2016-10-29 23:06:13 -0700

    Add API individual course.

--- Controller/CourseCatalogController.php
+++ Controller/CourseCatalogController.php
@@ -9,4 +9,8 @@ class CourseCatalogController extends APIController {
   public function indexAction($org, $term) {
     return $this->symfonyForward('KulaHEdSchedulingBundle:APIv1Course:courses', array('org' => $org, 'term' => $term));
   }  
+
+  public function courseAction($org, $term, $section_id) {
+    return $this->symfonyForward('KulaHEdSchedulingBundle:APIv1Course:course', array('org' => $org, 'term' => $term, 'section_id' => $section_id));
+  }  
 }
