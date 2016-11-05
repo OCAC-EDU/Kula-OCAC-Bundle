d0f311160fc9a2182f48bbfebee3d2b570a4babe
Makoa Jacobsen <makoa@makoajacobsen.com>
2016-11-05 22:39:12 +0000

    Extend OCAC for new scheduling API.

---
+++ Controller/SchedulingController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace OCACBundle\Controller;
+
+use Kula\Core\Bundle\FrameworkBundle\Controller\APIController;
+
+class SchedulingController extends APIController {
+  
+  public function addClassAction($student_id, $section_id) {
+    return $this->symfonyForward('KulaHEdSchedulingBundle:APIv1Schedule:addClass', array('student_id' => $student_id, 'section_id' => $section_id));
+  }  
+
+  public function removeClassAction($student_id, $class_id) {
+    return $this->symfonyForward('KulaHEdSchedulingBundle:APIv1Schedule:removeClass', array('student_id' => $student_id, 'class_id' => $class_id));
+  }  
+
+  public function getClassesAction($student_id, $org = null, $term = null) {
+     return $this->symfonyForward('KulaHEdSchedulingBundle:APIv1Schedule:getClasses', array('student_id' => $student_id, 'org' => $org, 'term' => $term));
+  }
+
+}
