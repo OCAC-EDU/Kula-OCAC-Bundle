b1d556589299e94b78ae9591ccd20284e4d1af88
Makoa Jacobsen <makoa@makoajacobsen.com>
2016-12-17 18:55:06 -0800

    Pass query parameters.

--- Controller/CourseCatalogController.php
+++ Controller/CourseCatalogController.php
@@ -7,10 +7,10 @@ use Kula\Core\Bundle\FrameworkBundle\Controller\APIController;
 class CourseCatalogController extends APIController {
   
   public function indexAction($org, $term) {
-    return $this->symfonyForward('KulaHEdSchedulingBundle:APIv1Course:courses', array('org' => $org, 'term' => $term));
+    return $this->symfonyForward('KulaHEdSchedulingBundle:APIv1Course:courses', array('org' => $org, 'term' => $term), $this->request->query->all());
   }  
 
   public function courseAction($org, $term, $section_id) {
-    return $this->symfonyForward('KulaHEdSchedulingBundle:APIv1Course:course', array('org' => $org, 'term' => $term, 'section_id' => $section_id));
+    return $this->symfonyForward('KulaHEdSchedulingBundle:APIv1Course:course', array('org' => $org, 'term' => $term, 'section_id' => $section_id), $this->request->query->all());
   }  
 }
