75543fce95fadd4de6c51ea6092a6d003d585d6b
Makoa Jacobsen <makoa@makoajacobsen.com>
2017-01-22 07:48:24 -0800

    Change K12 to HEd.

--- Controller/UserController.php
+++ Controller/UserController.php
@@ -7,11 +7,11 @@ use Kula\Core\Bundle\FrameworkBundle\Controller\APIController;
 class UserController extends APIController { 
 
   public function getChildrenAction() {
-     return $this->symfonyForward('KulaK12StudentBundle:APIv1Student:relatedChildren');
+     return $this->symfonyForward('KulaHEdStudentBundle:APIv1Student:relatedChildren');
   }
 
   public function addChildAction() {
-  	return $this->symfonyForward('KulaK12StudentBundle:APIv1Student:createChild');
+  	return $this->symfonyForward('KulaHEdStudentBundle:APIv1Student:createChild');
   }
 
 }
